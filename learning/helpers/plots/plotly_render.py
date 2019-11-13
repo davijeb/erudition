d40948daad78f2d1541aebf0c3548280ceb917cb
Jez Davies <jdavies86@bloomberg.net>
2019-11-13 15:52:26 +0000

    Ridge and Linear now seperated. Helper class extensions. Started on the SVC and have the IRIS dataset in Plotly and the SVC is working

--- learning/helpers/plots/plotly_render.py
+++ learning/helpers/plots/plotly_render.py
@@ -1,9 +1,9 @@
 import plotly
 from plotly import graph_objs as go
 
-def render(fig, width=500, height=500):
+def render(fig, title, width=500, height=500):
     
-    
+    fig["layout"]["title"] = title
     fig["layout"]["width"] = width
     fig["layout"]["height"] = height
     
@@ -12,10 +12,12 @@ def render(fig, width=500, height=500):
     fig["layout"]["yaxis"]["gridcolor"] = "#3F434F"
     fig["layout"]["xaxis"]["tickfont"]["color"] = "#ffffff"
     fig["layout"]["yaxis"]["tickfont"]["color"] = "#ffffff"
+
+    fig["layout"]["legend"]["font"]["color"] = "#ffffff"
     fig["layout"]["plot_bgcolor"] = "#292C34"
     fig["layout"]["paper_bgcolor"] = "#22252B"  
     
-    return fig
+    fig.show()
 
-def scatter( x, y, name, mode='lines', opacity=0.5, color=None):
-    return go.Scatter(x=x, y=y, mode =mode, opacity = 0.5, name = name, marker=dict( size=3, color=color ))
+def scatter( x, y, name, mode='lines', opacity=0.5, color=None, size=3):
+    return go.Scatter(x=x, y=y, mode =mode, opacity = opacity, name = name, marker=dict( size=size, color=color ))
--- learning/modules/GeneralizedLinearModels/helper/helper.py
+++ learning/modules/GeneralizedLinearModels/helper/helper.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from erudition.learning.helpers.plots.plotly_render import render, scatter
 
-def regression(df, power, linreg):
+def regression(df, power, linreg, plot_name=''):
 
     predictors = ['x']
 
@@ -12,7 +12,7 @@ def regression(df, power, linreg):
 
     linreg.fit(df[predictors], df['y'])
 
-    plot = scatter(df.x, linreg.predict(df[predictors]), 'Raw')
+    plot = scatter(df.x, linreg.predict(df[predictors]), plot_name)
 
     #Return the result in pre-defined format
     rss = sum((linreg.predict(df[predictors])-df.y)**2)
@@ -21,3 +21,9 @@ def regression(df, power, linreg):
     ret.extend(linreg.coef_)
 
     return plot, ret
+
+def data_sin_wave():
+    x = np.array([i*np.pi/180 for i in range(30,300,4)])
+    y = np.sin(x) + np.random.normal(0,0.15,len(x))
+
+    return pd.DataFrame(np.column_stack([x,y]),columns=['x','y'])
