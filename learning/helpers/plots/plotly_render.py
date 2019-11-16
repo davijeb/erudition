e2850c9d0e82f9ecd2fada8c50c81e31ff5c21c5
Jez Davies <jerdavies@gmail.com>
2019-11-16 19:05:55 +0000

    Added a new SVM Proof notebook that showed how the support vectors were created using a two-classifier system. Need to add the contour plot as before to show the actual margins between the sets of points

--- learning/helpers/plots/plotly_render.py
+++ learning/helpers/plots/plotly_render.py
@@ -13,9 +13,11 @@ def render(fig, title, width=500, height=500, x_axis_title=None, y_axis_title=No
     
     fig["layout"]["title"]["font"]["color"] = "#9398A4"
     fig["layout"]["xaxis"]["gridcolor"] = "#3F434F"
+    fig["layout"]["xaxis"]["zeroline"] = False
     fig["layout"]["yaxis"]["gridcolor"] = "#3F434F"
     fig["layout"]["xaxis"]["tickfont"]["color"] = "#ffffff"
     fig["layout"]["yaxis"]["tickfont"]["color"] = "#ffffff"
+    fig["layout"]["yaxis"]["zeroline"] = False
 
     fig["layout"]["legend"]["font"]["color"] = "#ffffff"
     fig["layout"]["plot_bgcolor"] = "#292C34"
@@ -25,3 +27,15 @@ def render(fig, title, width=500, height=500, x_axis_title=None, y_axis_title=No
 
 def scatter( x, y, name, mode='lines', opacity=0.5, color=None, size=3):
     return go.Scatter(x=x, y=y, mode =mode, opacity = opacity, name = name, marker=dict( size=size, color=color ))
+
+def shape(x,y, type='circle', line_color='LightSeaGreen', size=0.1):
+    return go.layout.Shape(
+        type=type,
+        xref="x",
+        yref="y",
+        x0=x-size,
+        y0=y-size,
+        x1=x+size,
+        y1=y+size,
+        line_color="LightSeaGreen"
+    )
